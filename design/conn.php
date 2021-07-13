143300cdb96227ce42ce78050ed450de6763c9d7
Payton-Lee <69508812+Payton-Lee@users.noreply.github.com>
2021-07-14 00:50:18 +0800

    Delete design directory

--- design/conn.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    $mySQLi = new MySQLi('localhost','root','123456','mingrenwang',3306);
-    if($mySQLi -> connect_errno){
-        die('连接错误' . $mySQLi -> connect_error);
-    }
-    $mySQLi -> set_charset('utf8');
-?>
